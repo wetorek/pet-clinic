28d4e9dd633f1f7c25035ca9a1b394f2c32a17c4
bmzyk <bmzyk@griddynamics.com>
2021-03-15 21:33:01 +0100

    Test database configuration created

--- src/test/java/com/clinic/pet/petclinic/PetClinicApplicationTests.java
+++ src/test/java/com/clinic/pet/petclinic/PetClinicApplicationTests.java
@@ -2,8 +2,10 @@ package com.clinic.pet.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PetClinicApplicationTests {
 
     @Test
