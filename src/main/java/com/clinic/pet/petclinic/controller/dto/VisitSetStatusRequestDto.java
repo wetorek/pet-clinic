a17203030ede859653559ed718a6d984aa45f264
bmzyk <bmzyk@griddynamics.com>
2021-03-25 18:50:39 +0100

    flyway migration refactored

--- src/main/java/com/clinic/pet/petclinic/controller/dto/VisitSetStatusRequestDto.java
+++ src/main/java/com/clinic/pet/petclinic/controller/dto/VisitSetStatusRequestDto.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class VisitSetStatusRequestDto {
-    private  String status;
+    private String status;
 }
--- src/main/resources/db/migration/V1__init_task_tables.sql
+++ src/main/resources/db/migration/V1__init_task_tables.sql
@@ -1,34 +1,44 @@
-CREATE TABLE IF NOT EXISTS customers(
+-- drop table customers cascade;
+-- drop table animals cascade;
+-- drop table vets cascade;
+-- drop table visits cascade;
+-- drop table flyway_schema_history cascade;
 
-    id integer primary key serial not null,
-    name varchar(255),
-    surname varchar(255)
+CREATE TABLE IF NOT EXISTS customers
+(
+    id      serial primary key not null,
+    name    varchar(255)       not null,
+    surname varchar(255)       not null
 );
 
-CREATE TABLE IF NOT EXISTS animals(
-    id integer primary key serial not null,
+CREATE TABLE IF NOT EXISTS animals
+(
+    id            serial primary key not null,
     date_of_birth date,
-    name varchar (255),
-    species varchar(255),
-    owner_id integer not null,
-    CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES customers(id)
+    name          varchar(255)       not null,
+    species       varchar(255)       not null,
+    owner_id      integer            not null,
+    FOREIGN KEY (owner_id) REFERENCES customers (id)
 );
-CREATE TABLE IF NOT EXISTS vets(
-    id integer primary key serial not null ,
-    availability varchar(255),
-    image oid, -- ?
-    name varchar (255),
-    surname varchar (255)
+CREATE TABLE IF NOT EXISTS vets
+(
+    id           serial primary key not null,
+    availability varchar(255)       not null,
+    image        oid,
+    name         varchar(255)       not null,
+    surname      varchar(255)       not null
 );
-CREATE TABLE IF NOT EXISTS visits(
-    id integer primary key serial not null,
-    descriptions varchar(255),
-    price  numeric(19, 2) not null,
-    start_time timestamp not null,
-    status varchar(255) not null,
-    animal_id integer not null,
-    customer_id integer not null,
-    vet_id integer not null,
-    CONSTRAINT fk_owner_visit FOREIGN KEY (customer_id) REFERENCES customers(id),
-    CONSTRAINT fk_animal_visit FOREIGN KEY (animal_id) REFERENCES animals(id)
+CREATE TABLE IF NOT EXISTS visits
+(
+    id          serial primary key not null,
+    description varchar(255),
+    price       numeric(19, 2)     not null,
+    start_time  timestamp          not null,
+    status      varchar(255)       not null,
+    animal_id   integer            not null,
+    customer_id integer            not null,
+    vet_id      integer            not null,
+    FOREIGN KEY (customer_id) REFERENCES customers (id),
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
 )
