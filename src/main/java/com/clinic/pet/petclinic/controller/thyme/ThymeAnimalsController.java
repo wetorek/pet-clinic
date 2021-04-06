ed9b82b0db5c9df680ad67109fe96c237aacb918
kamilsuchy <44306803+kamilsuchy@users.noreply.github.com>
2021-04-06 17:10:28 +0200

    thyme animals controller added

--- src/main/java/com/clinic/pet/petclinic/controller/thyme/ThymeAnimalsController.java
+++ src/main/java/com/clinic/pet/petclinic/controller/thyme/ThymeAnimalsController.java
@@ -1,6 +1,6 @@
 package com.clinic.pet.petclinic.controller.thyme;
 
-import com.clinic.pet.petclinic.service.CustomerService;
+import com.clinic.pet.petclinic.service.AnimalService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/ui/customers")
+@RequestMapping("/ui/animals")
 @AllArgsConstructor
-public class ThymeCustomerController {
-
-    private final CustomerService customerService;
+public class ThymeAnimalsController {
+    private final AnimalService animalService;
 
     @GetMapping
-    public String getAllCustomers(Model model) {
-        model.addAttribute("customers", customerService.getAllCustomers());
-        return "customers";
+    public String getAllAnimals(Model model) {
+        model.addAttribute("animals", animalService.getAllAnimals());
+        return "animals";
     }
 
 }
