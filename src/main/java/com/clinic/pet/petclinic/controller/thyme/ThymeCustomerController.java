eaaaccd69e0ef2d9e7bb24e8d0af094a981a2172
kamilsuchy <44306803+kamilsuchy@users.noreply.github.com>
2021-04-06 16:59:21 +0200

    customer thymeleaf controller added

---
+++ src/main/java/com/clinic/pet/petclinic/controller/thyme/ThymeCustomerController.java
@@ -0,0 +1,24 @@
+package com.clinic.pet.petclinic.controller.thyme;
+
+import com.clinic.pet.petclinic.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ui/customers")
+@AllArgsConstructor
+public class ThymeCustomerController {
+
+    private final CustomerService customerService;
+
+
+    @GetMapping
+    public String getAllCustomers(Model model) {
+        model.addAttribute("customers", customerService.getAllCustomers());
+        return "customers";
+    }
+
+}
